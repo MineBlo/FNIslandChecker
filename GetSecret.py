27f3665b45dcb1954c79e4629db0a555e3eb5f6c
MineBlo <badbaird30@gmail.com>
2024-11-13 12:10:41 +0200

    Update GetSecret.py

--- GetSecret.py
+++ GetSecret.py
@@ -1,8 +1,8 @@
 import time
 import requests
 
-AuthorizationCode = input("Go here (https://www.epicgames.com/id/api/redirect?clientId=3446cd72694c4a4485d81b77adbb2141&responseType=code)\nAnd paste the text found next to 'AuthorizationCode'. You will need to be logged in the browser with the account that will be doing the web requests (DO NOT USE YOUR MAIN ACCOUNT):")
-bearerresponse = requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token", data = {'grant_type': 'authorization_code', 'code':AuthorizationCode}, headers = {"Content-Type": "application/x-www-form-urlencoded","Authorization":"basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE="}).json()
+AuthorizationCode = input("Go here (https://www.epicgames.com/id/api/redirect?clientId=af43dc71dd91452396fcdffbd7a8e8a9&responseType=code)\nAnd paste the text found next to 'AuthorizationCode'. You will need to be logged in the browser with the account that will be doing the web requests (DO NOT USE YOUR MAIN ACCOUNT):")
+bearerresponse = requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token", data = {'grant_type': 'authorization_code', 'code':AuthorizationCode}, headers = {"Content-Type": "application/x-www-form-urlencoded","Authorization":"basic YWY0M2RjNzFkZDkxNDUyMzk2ZmNkZmZiZDdhOGU4YTk6NFlYdlNFQkxGUlBMaDFoekdaQWtmT2k1bXF1cEZvaFo="}).json()
 EpicID=bearerresponse['account_id']
 Bearer=bearerresponse['access_token']
 DeviceAuthResponse=requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/public/account/"+EpicID+"/deviceAuth", headers = {"Authorization": "Bearer "+ str(Bearer)})
