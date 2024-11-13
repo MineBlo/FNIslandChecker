4e26eab25d71ae475aeb3cf15070b96b83ed6481
MineBlo <badbaird30@gmail.com>
2024-11-13 12:24:16 +0200

    Update GetSecret.py

--- GetSecret.py
+++ GetSecret.py
@@ -7,8 +7,8 @@ EpicID=bearerresponse['account_id']
 Bearer=bearerresponse['access_token']
 DeviceAuthResponse=requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/public/account/"+EpicID+"/deviceAuth", headers = {"Authorization": "Bearer "+ str(Bearer)})
 print("!!!!!!!!!!! NEVER SHARE THESE WITH ANYONE NOT EVEN ME AND DONT USE YOUR MAIN ACCOUNT TO GET THESE !!!!!!!!!!!!!!")
-print("DEVICE ID"+DeviceAuthResponse.json()['deviceId'])
-print("EPIC ID:"+DeviceAuthResponse.json()['accountId'])
-print("SECRET ID"+DeviceAuthResponse.json()['secret'])
+print("DEVICE ID: "+DeviceAuthResponse.json()['deviceId'])
+print("EPIC ID: "+DeviceAuthResponse.json()['accountId'])
+print("SECRET ID: "+DeviceAuthResponse.json()['secret'])
 print("!!!!!!!!!!! NEVER SHARE THESE WITH ANYONE NOT EVEN ME AND DONT USE YOUR MAIN ACCOUNT TO GET THESE !!!!!!!!!!!!!!")
 input()
